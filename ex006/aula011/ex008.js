62bfa20ac21353117966bcd169cae6d763fbfab9
LeviSoares190 <dev.levi.soares190@gmail.com>
2025-07-11 23:24:10 -0300

    Condicionais

---
+++ ex006/aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel  = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h por hora`)
+if (vel >= 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!!`)
+}
+else{
+    console.log(`Seu carro esta na velocidade média. Boa viagem!!`)
+}
+console.log('Diriga sempre usando cinto de segurança!')
